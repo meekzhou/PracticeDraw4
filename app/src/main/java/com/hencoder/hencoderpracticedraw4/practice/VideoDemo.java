bc9ee5d2d1e7ed9869315bc860be672066f334e8
witt <meekzhou@gmail.com>
2017-10-12 16:10:53 +0800

    左右翻页

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/VideoDemo.java
@@ -0,0 +1,102 @@
+package com.hencoder.hencoderpracticedraw4.practice;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+import com.hencoder.hencoderpracticedraw4.R;
+
+public class VideoDemo extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
+    Camera camera = new Camera();
+    float rotationY;
+
+    ObjectAnimator animator = ObjectAnimator.ofFloat(this, "rotationY", 0, -180);
+
+
+    public VideoDemo(Context context) {
+        super(context);
+    }
+
+    public VideoDemo(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VideoDemo(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+
+        animator.setDuration(2000);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        animator.start();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        animator.end();
+    }
+
+    @Override
+    public void setRotationY(float rotationY) {
+        this.rotationY = rotationY;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+        int x = centerX - bitmapWidth / 2;
+        int y = centerY - bitmapHeight / 2;
+
+        //原点
+        int left = (getWidth() - bitmap.getWidth()) / 2;
+        int top = (getHeight() - bitmap.getHeight()) / 2;
+
+
+        canvas.save();
+        canvas.clipRect(left, 0, left + bitmapWidth / 2, getHeight());
+        canvas.drawBitmap(bitmap, x, y, paint);
+        canvas.restore();
+
+        canvas.save();
+        if (rotationY > -90) {
+            canvas.clipRect(left + bitmapWidth / 2, 0, getWidth(), getHeight());
+        } else {
+            canvas.clipRect(left, 0, left + bitmapWidth / 2, getHeight());
+        }
+        camera.save();
+        camera.rotateY(rotationY);
+        canvas.translate(centerX, centerY);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-centerX, -centerY);
+        camera.restore();
+        canvas.drawBitmap(bitmap, x, y, paint);
+        canvas.restore();
+    }
+}
